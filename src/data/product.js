58e0102b5ceb7cea587f5db7f1030139e2d42c90
anjalifredy-ai <anjalifredy@gmail.com>
2026-06-04 17:52:45 +0530

    Create product.js

---
+++ src/data/product.js
@@ -0,0 +1,53 @@
+export const products = [
+  {
+    id: 1,
+    name: "iPhone 16",
+    price: "₹79,999",
+    image: "https://picsum.photos/400/300?1",
+    stores: {
+      amazon: "https://amazon.in",
+      flipkart: "https://flipkart.com",
+      myntra: "https://myntra.com",
+      meesho: "https://meesho.com"
+    }
+  },
+
+  {
+    id: 2,
+    name: "Samsung Galaxy S26",
+    price: "₹69,999",
+    image: "https://picsum.photos/400/300?2",
+    stores: {
+      amazon: "https://amazon.in",
+      flipkart: "https://flipkart.com",
+      myntra: "https://myntra.com",
+      meesho: "https://meesho.com"
+    }
+  },
+
+  {
+    id: 3,
+    name: "Boat Headphones",
+    price: "₹1,999",
+    image: "https://picsum.photos/400/300?3",
+    stores: {
+      amazon: "https://amazon.in",
+      flipkart: "https://flipkart.com",
+      myntra: "https://myntra.com",
+      meesho: "https://meesho.com"
+    }
+  },
+
+  {
+    id: 4,
+    name: "Gaming Mouse",
+    price: "₹999",
+    image: "https://picsum.photos/400/300?4",
+    stores: {
+      amazon: "https://amazon.in",
+      flipkart: "https://flipkart.com",
+      myntra: "https://myntra.com",
+      meesho: "https://meesho.com"
+    }
+  }
+];
